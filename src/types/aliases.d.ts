fae4c09e8d16b1c1a764f0907fc5f3e4fe465032
gaetanlegac <contact@gaetan-legac.fr>
2022-11-29 10:34:44 +0100

    Cleanup outdated code

--- src/types/aliases.d.ts
+++ src/types/aliases.d.ts
@@ -7,17 +7,6 @@ declare module "@client/pages/\*.tsx" {
     export = value;
 }
 
-// Basic Models
-declare module "@/server/models/User" {
-    const User: import("../common/models").User;
-    export = User;
-}
-
-declare module "@/server/models/IP" {
-    const IP: import("../common/models").IP;
-    export = IP;
-}
-
 declare module "@/server/services/auth" {
     const UserAuthService: import("../server/services/auth/base").default;
     export = UserAuthService;
