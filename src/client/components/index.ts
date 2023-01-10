398de6e3919104591c85e6451039ab8883cfab63
gaetanlegac <contact@gaetan-legac.fr>
2023-01-10 21:16:10 +0100

    Export input components from @client/components

--- src/client/components/index.ts
+++ src/client/components/index.ts
@@ -5,4 +5,15 @@ export { default as Table } from './Table';
 export { default as Select } from './Select';
 export { default as Amount } from './Amount';
 export { default as Logo } from './Logo';
+
 export { default as Input } from './input';
+export { default as Textarea } from './input/Textarea';
+export { default as Number } from './input/Number';
+export { default as Couleur } from './input/Couleur';
+export { default as Slider } from './input/Slider';
+export { default as Upload } from './input/Upload';
+export { default as Code } from './input/Code';
+export { default as Radio } from './input/Radio';
+export { default as Rte } from './input/Rte';
+export { default as Date } from './input/Date';
+export { default as Periode } from './input/Periode';
