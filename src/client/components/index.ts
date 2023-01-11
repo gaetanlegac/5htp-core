7097de90a92f3b9a5b6d25e97d92fd3b7a632e86
gaetanlegac <contact@gaetan-legac.fr>
2023-01-11 17:12:51 +0100

    Client components: fix import uppercase

--- src/client/components/index.ts
+++ src/client/components/index.ts
@@ -1,6 +1,6 @@
 export { default as Button } from './button';
-export { default as Row } from './row';
-export { default as Card } from './card';
+export { default as Row } from './Row';
+export { default as Card } from './Card';
 export { default as Table } from './Table';
 export { default as Select } from './Select';
 export { default as Amount } from './Amount';
