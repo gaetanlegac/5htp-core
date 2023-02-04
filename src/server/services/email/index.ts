b0c779dffd429d3df73e1e90ba1f12bae9d087b1
gaetanlegac <contact@gaetan-legac.fr>
2023-02-04 17:49:20 +0100

    Email service: add support for markdown and person name

--- src/server/services/email/index.ts
+++ src/server/services/email/index.ts
@@ -8,6 +8,7 @@
 
 // Core
 import Application, { Service } from '@server/app';
+import markdown from '@common/data/markdown';
 
 // Speciic
 import { jsonToHtml } from './utils';
@@ -17,18 +18,21 @@ import type { Transporter } from './transporter';
 - SERVICE CONFIG
 ----------------------------------*/
 
+const LogPrefix = `[services][email]`
+
 export type Config = {
     debug: boolean,
+    simulateWhenLocal: boolean,
     default: {
         transporter: string,
-        from: string
+        from: TPerson
     },
     transporters: {
         [transporterName: string]: Transporter
     },
     bugReport: {
-        from: string,
-        to: string
+        from: TPerson,
+        to: TPerson
     }
 }
 
@@ -42,11 +46,17 @@ export type Hooks = {
 
 export { Transporter } from './transporter';
 
-export type TEmail = THtmlEmail | TTemplateEmail;
+export type TEmail = THtmlEmail | TMarkdownEmail// | TTemplateEmail;
+
+type TPerson = {
+    name?: string,
+    email: string
+}
 
 type TBaseEmail = { 
-    to: string | string[], 
-    from?: string 
+    to: TPerson | TPerson[], 
+    cc?: TPerson | TPerson[]
+    from?: TPerson,
 };
 
 export type THtmlEmail = TBaseEmail & {
@@ -54,22 +64,41 @@ export type THtmlEmail = TBaseEmail & {
     html: string | { [label: string]: any },
 }
 
-export type TTemplateEmail = TBaseEmail & {
-    template: keyof typeof templates,
-    data?: TObjetDonnees
+export type TMarkdownEmail = TBaseEmail & {
+    subject: string,
+    markdown: string,
 }
 
+/*export type TTemplateEmail = TBaseEmail & {
+    template: keyof typeof templates,
+    data?: TObjetDonnees
+}*/
+
 export type TCompleteEmail = With<THtmlEmail, {
-    to: string[],
-    from: string,
+    to: TPerson[],
+    from: TPerson,
+    cc: TPerson[]
 }>;
+
+type TShortEmailSendArgs =  [
+    to: string, 
+    subject: string, 
+    markdown: string, 
+    options?: TOptions
+]
+
+type TCompleteEmailSendArgs = [
+    emails: TEmail | TEmail[], 
+    options?: TOptions
+]
+
+type TEmailSendArgs = TShortEmailSendArgs | TCompleteEmailSendArgs;
 
 /*----------------------------------
 - TYPES: OPTIONS
 ----------------------------------*/
 type TOptions = {
-    transporter?: string,
-    testing?: boolean
+    transporter?: string
 }
 
 /*----------------------------------
@@ -88,30 +117,52 @@ export default class Email extends Service<Config, Hooks, Application> {
     }
 
 
-    public async send(
-        emails: TEmail | TEmail[],
-        options: TOptions = {}
-    ): Promise<void> {
+    public async send( to: string, subject: string, markdown: string, options?: TOptions );
+    public async send( emails: TEmail | TEmail[], options?: TOptions ): Promise<void>;
+    public async send( ...args: TEmailSendArgs ): Promise<void> {
 
-        // Normalisation en tableau
-        if (!Array.isArray( emails ))
-            emails = [emails];
+        let emails: TEmail[] | TEmail;
+        let options: TOptions | undefined;
+        if (typeof args[0] === 'string') {
+            const [to, subject, markdown, opts] = args as TShortEmailSendArgs;
+            emails = [{
+                to: { email: to },
+                subject,
+                markdown
+            }]
+            options = opts;
+        } else {
+
+            ([ emails, options ] = args as TCompleteEmailSendArgs);
+            if (!Array.isArray( emails ))
+                emails = [emails];
+        }
+
+        options = options || {}
 
         this.config.debug && console.log(`Preparing to send ${emails.length} emails ...`);
 
+        const htmlWarning = this.app.env.profile === 'dev'
+            ? `⚠️ This email has been sent for testing purposes. Please ignore it if you're not a developer.<br /><br />`
+            : '';
+
         const emailsToSend: TCompleteEmail[] = emails.map(email => {
 
-            const from = email.from === undefined
+            const from: TPerson = email.from === undefined
                 ? this.config.default.from
                 : email.from;
 
-            const to = typeof email.to === 'string'
-                ? [email.to]
-                : email.to;
+            const cc: TPerson[] = email.cc === undefined ? [] : Array.isArray(email.cc)
+                ? email.cc
+                : [email.cc];
+
+            const to: TPerson[] = Array.isArray(email.to)
+                ? email.to
+                : [email.to];
 
             // Via template
             // TODO: Restore templates feature
-            if ('template' in email) {
+            /*if ('template' in email) {
 
                 const template = templates[email.template];
 
@@ -126,20 +177,32 @@ export default class Email extends Service<Config, Hooks, Application> {
                     ...email,
                     // Vire le "> " au début
                     subject: txt.substring(2, delimTitre),
-                    html: txt.substring(delimTitre + 2),
+                    html: htmlWarning + txt.substring(delimTitre + 2),
                     from,
-                    to
+                    to,
+                    cc
                 }
 
-            }
+            } else */if ('markdown' in email) {
 
-            return {
-                ...email,
-                html: typeof email.html === "string" 
-                    ? email.html 
-                    : jsonToHtml(email.html),
-                from,
-                to
+                return {
+                    ...email,
+                    html: htmlWarning + markdown.render(email.markdown),
+                    from,
+                    to,
+                    cc
+                }
+
+            } else {
+                return {
+                    ...email,
+                    html: htmlWarning + (typeof email.html === "string" 
+                        ? email.html 
+                        : jsonToHtml(email.html)),
+                    from,
+                    to,
+                    cc
+                }
             }
             
         });
@@ -148,17 +211,14 @@ export default class Email extends Service<Config, Hooks, Application> {
         if (transporterName === undefined)
             throw new Error(`Please define at least one mail transporter.`);
 
-        console.info(`Sending ${emailsToSend.length} emails via transporter "${transporterName}"`, emailsToSend[0].subject);
+        console.info(LogPrefix, `Sending ${emailsToSend.length} emails via transporter "${transporterName}"`, emailsToSend[0].subject);
 
         // Pas d'envoi d'email quand local
-        /*if (app.env.name === 'local' && options.testing !== false) {
-            console.log(`Email send canceled: Test mode enabled, or you're in local`, { 
-                'options.testing': options.testing,
-                'app.env.name': app.env.name,
-            });
+        if (this.app.env.name === 'local' && this.config.simulateWhenLocal === true) {
+            console.log(LogPrefix, `Simulate email sending:\n`, emailsToSend[0].html);
             return;
-        } else */if (emailsToSend.length === 0) {
-            console.log(`No email to send.`);
+        } else if (emailsToSend.length === 0) {
+            console.warn(LogPrefix, `No email to send.`);
             return;
         }
 
