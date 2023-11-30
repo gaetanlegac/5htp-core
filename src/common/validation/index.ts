5c0ba4f0f58c02718017cc89afc8721ab198ea7b
gaetanlegac <contact@gaetan-legac.fr>
2023-11-30 20:39:18 +0100

    @common/validators: export TSchemaFields

--- src/common/validation/index.ts
+++ src/common/validation/index.ts
@@ -1,4 +1,4 @@
 export { default as Schema } from './schema';
-export type { TSchemaFields } from './schema';
+export type { TSchemaFields, TValidatedData } from './schema';
 export { default as Validators } from './validators';
 export { default as Validator } from './validator';
