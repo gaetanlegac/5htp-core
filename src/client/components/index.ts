d4efeb071f45314adcc40df0a6f8a68371ff4d53
gaetanlegac <contact@gaetan-legac.fr>
2024-07-13 11:57:21 +0200

    Remove old components

--- src/client/components/index.ts
+++ src/client/components/index.ts
@@ -12,14 +12,12 @@ export { default as Row } from './Row';
 export { default as Logo } from './logo';
 export { default as Video } from './Video';
 
-// Input
-export { default as Number } from './input/Number';
-export { default as Slider } from './input/Slider';
-export { default as Radio } from './input/Radio';
-export { default as Checkbox } from './input/Checkbox';
+// Input (OLD, TO ADAPT)
+//export { default as Slider } from './input/Slider';
+//export { default as Radio } from './input/Radio';
+//export { default as Checkbox } from './input/Checkbox';
 
 // Data
-export { default as Amount } from './Amount';
 export { default as Time } from './data/Time';
 export { default as SpinText } from './data/spintext';
 export { default as Progressbar } from './data/progressbar';
@@ -30,12 +28,3 @@ export { default as Select } from './Select';
 export { default as Input } from './inputv3';
 export { default as File } from './inputv3/file';
 export { default as DateRangeInput } from './inputv3/date';
-
-// TOD: fix popover component
-//export { default as Date } from './input/Date';
-//export { default as Periode } from './input/Periode';
-
-// TODO: adapt
-//export { default as Couleur } from './input/Couleur';
-//export { default as Code } from './input/Code';
-//export { default as Rte } from './input/Rte';
