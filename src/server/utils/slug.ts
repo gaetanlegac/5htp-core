c0de1752bfee18b21cf713cc83280264a5080a8e
gaetanlegac <contact@gaetan-legac.fr>
2024-11-19 10:36:58 +0100

    Slug generator: remove special characters and numbers

--- src/server/utils/slug.ts
+++ src/server/utils/slug.ts
@@ -26,9 +26,9 @@ export class Slug {
         // Generate slug
         let slug = slugify(label, {
             replacement: '-',  // replace spaces with replacement character, defaults to `-`
-            remove: undefined, // remove characters that match regex, defaults to `undefined`
+            remove: /[^a-z\s]/ig, // remove characters that match regex, defaults to `undefined`
             lower: true,      // convert to lower case, defaults to `false`
-            strict: false,     // strip special characters except replacement, defaults to `false`
+            strict: true,     // strip special characters except replacement, defaults to `false`
             locale: 'vi',       // language code of the locale to use
             trim: true         // trim leading and trailing replacement chars, defaults to `true`
         });
