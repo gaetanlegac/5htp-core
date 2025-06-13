2421c8382c1cdc5d87e7efb18a2cefa65116d103
gaetanlegac <contact@gaetan-legac.fr>
2025-06-13 13:03:31 +0200

    Dropdown: use 5HTP components

--- client/components/DropDown.tsx
+++ client/components/DropDown.tsx
@@ -5,8 +5,9 @@
 // Npm
 import React from 'react';
 import { JSX, ComponentChild } from 'preact';
-import { Button, Menu, MenuProps } from '@mantine/core';
-import { Props as ButtonProps } from './Button';
+import { Menu, MenuProps } from '@mantine/core';
+import Button, { Props as ButtonProps } from './Button';
+import Popover, { Props as PopoverProps } from './containers/Popover';
 
 // Core libs
 import { useMantineInput, InputBaseProps } from './utils';
@@ -26,6 +27,21 @@ export default ({
     label, children, 
     menuProps = {}, size, icon, ...btnProps 
 }: Props) => {
+    return (
+        <Popover content={(
+            <div class="card bg white col menu">
+                {children}
+            </div>
+        )}>
+            <Button {...btnProps} 
+                size={size}
+                suffix={<i src="angle-down" />}
+            >
+                {icon && <i src={icon} />}
+                {label}
+            </Button>
+        </Popover>
+    )
     return (
         <Menu {...menuProps} size={size}>
             <Menu.Target>
