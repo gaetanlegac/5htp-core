f017b1e5297f0fe66954bb62258ece517ebf9e94
gaetanlegac <contact@gaetan-legac.fr>
2025-07-25 09:25:33 +0200

    Facet: add support for withStats

--- server/services/prisma/Facet.ts
+++ server/services/prisma/Facet.ts
@@ -1,21 +1,34 @@
-
-import type { Prisma } from '@models/types';
+import type { Prisma, PrismaClient } from '@models/types';
 import * as runtime from '@/var/prisma/runtime/library.js';
+
+export type TWithStats = {
+    $table: string,
+    $key: string
+} & {
+    [key: string]: string // key => SQL
+}
+
+export type TSubset = (...a: any[]) => Prisma.ProspectContactLeadFindFirstArgs & {
+    withStats?: TWithStats
+}
 
 export default class Facet<
     D extends {
         findMany(args?: any): Promise<any>
         findFirst(args?: any): Promise<any>
     },
-    S extends (...a: any[]) => Prisma.ProspectContactLeadFindFirstArgs,
+    S extends TSubset,
     R
 > {
     constructor(
+
+        private readonly prisma: PrismaClient,
+
         private readonly delegate: D,
         private readonly subset: S,
 
         /* the **ONLY** line that changed ↓↓↓ */
-        private readonly transform: (
+        private readonly transform?: (
             row: runtime.Types.Result.GetResult<
                 Prisma.$ProspectContactLeadPayload,
                 ReturnType<S>,
@@ -24,19 +37,82 @@ export default class Facet<
         ) => R,
     ) { }
 
-    public findMany(
+    public async findMany(
         ...args: Parameters<S>
     ): Promise<R[]> {
-        return this.delegate
-            .findMany(this.subset(...args))
-            .then(rows => rows.map(this.transform))
+
+        const { withStats, ...subset } = this.subset(...args);
+
+        const results = await this.delegate.findMany(subset);
+
+        // Load stats
+        const stats = withStats 
+            ? await this.fetchStats( withStats, results )
+            : [];
+
+        return results.map(row => this.transformResult(row, stats, withStats));
     }
 
-    public findFirst(
+    public async findFirst(
         ...args: Parameters<S>
     ): Promise<R | null> {
-        return this.delegate
-            .findFirst(this.subset(...args))
-            .then(result => result ? this.transform(result) : null)
+
+        const { withStats, ...subset } = this.subset(...args);
+
+        const result = await this.delegate.findFirst(subset);
+
+        const stats = withStats 
+            ? await this.fetchStats( withStats, [result] )
+            : [];
+
+        return result ? this.transformResult(result, stats, withStats) : null;
+    }
+
+    private async fetchStats(
+        { $table, $key, ...withStats }: TWithStats,
+        results: any[]
+    ): Promise<any[]> {
+
+        const select = Object.entries(withStats).map(([key, sql]) =>    
+            `(COALESCE((
+                ${sql}
+            ), 0)) as ${key}`
+        );
+
+        const stats = await this.prisma.$queryRawUnsafe(`
+            SELECT ${$key}, ${select.join(', ')} 
+            FROM ${$table} 
+            WHERE ${$key} IN (
+                ${results.map(r => "'" + r[ $key ] + "'").join(',')}
+            )
+        `);
+
+        for (const stat of stats) {
+            for (const key in stat) {
+    
+                if (key === $key)
+                    continue;
+    
+                stat[key] = stat[key] ? parseInt(stat[key]) : 0;
+            }
+        }
+
+        return stats;
+    }
+
+    private transformResult( result: any, stats: any[], withStats?: TWithStats ) {
+
+        // Transform stats
+        const resultStats = withStats
+            ? stats.find(stat => stat[withStats.$key] === result[withStats.$key]) || {}
+            : {};
+
+        if (this.transform)
+            result = this.transform(result);
+
+        return {
+            ...result,
+            ...resultStats
+        }
     }
 }
